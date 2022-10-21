fef3453137e49b81e5c989eaea67cc9233f2f8fb
Minghan Zhu <mz2716@columbia.edu>
2022-10-21 02:22:15 -0400

    Create AccountSetting.java

---
+++ src/main/java/com/example/imdbplus/entity/AccountSetting.java
@@ -0,0 +1,19 @@
+package com.example.imdbplus.entity;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamoDBDocument
+public class AccountSetting {
+
+    @DynamoDBAttribute
+    private Boolean isPrivate;
+    @DynamoDBAttribute
+    private Boolean isAdult;
+}
