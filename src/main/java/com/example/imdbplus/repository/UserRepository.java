fa3069f422c835d40b80706813d9c523a5ab603a
Minghan Zhu <mz2716@columbia.edu>
2022-10-21 02:47:10 -0400

    Update UserRepository.java

--- src/main/java/com/example/imdbplus/repository/UserRepository.java
+++ src/main/java/com/example/imdbplus/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.imdbplus.repository;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
 import com.example.imdbplus.entity.User;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.List;
 
 @Repository
 public class UserRepository {
@@ -19,23 +21,19 @@ public class UserRepository {
     public String save(User user) {
         // Add new user to the database if the username does not exist
         HashMap<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+        // scan the table to see if the username exists
         eav.put(":v1", new AttributeValue().withS(user.getUsername()));
-        DynamoDBQueryExpression<User> queryExpression = new DynamoDBQueryExpression<User>()
-                .withKeyConditionExpression("username = :v1")
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("username = :v1")
                 .withExpressionAttributeValues(eav);
-        if (dynamoDBMapper.query(User.class, queryExpression).isEmpty()) {
+        // if the username does not exist, add the user to the database
+        List<User> replies = dynamoDBMapper.scan(User.class, scanExpression);
+        if (replies.size() == 0) {
             dynamoDBMapper.save(user);
             return "User saved successfully";
         } else {
             return "Username already exists";
         }
-
-//        if (dynamoDBMapper.load(User.class, user.getUsername()) == null) {
-//            dynamoDBMapper.save(user);
-//            return new String(user.getUsername() + " has been added to the database.");
-//        } else {
-//            return "Username already exists";
-//        }
     }
 
     public User getUser(String userId) {
