8e9b6c1cd7b433e1daa2cc73b8cfbf492768b7ec
Minghan Zhu <mz2716@columbia.edu>
2022-10-21 02:21:58 -0400

    Create DynamoDBConfig.java

---
+++ src/main/java/com/example/imdbplus/config/DynamoDBConfig.java
@@ -0,0 +1,40 @@
+package com.example.imdbplus.config;
+
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDBConfig {
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        return new DynamoDBMapper(amazonDynamoDB());
+    }
+
+    private AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                "dynamodb.us-east-1.amazonaws.com",
+                                "us-east-1"
+                        )
+                )
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(
+                                new BasicAWSCredentials(
+                                        "AKIATEAVDARUDWHF3AES",
+                                        "4AUPrMzfY7d3avEKyc5nBq3m2vLcr02pB2IN+dzb"
+                                )
+                        )
+                )
+                .build();
+    }
+}
