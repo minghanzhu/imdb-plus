d5ac9f6c47b230fdf4a778b3c697641c6d472f45
Minghan Zhu <mz2716@columbia.edu>
2022-10-20 22:07:49 -0400

    Spring Initializr

---
+++ src/main/java/com/example/imdbplus/ImdbPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.imdbplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imdbplus/ImdbPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imdbplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
