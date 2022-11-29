c9139f0d6785c280332bec14109785de77a976fd
Minghan Zhu <mz2716@columbia.edu>
2022-11-29 17:05:25 -0500

    Update ImdbPlusApplicationTests.java

--- src/test/java/com/example/imdbplus/ImdbPlusApplicationTests.java
+++ src/test/java/com/example/imdbplus/ImdbPlusApplicationTests.java
@@ -2,13 +2,16 @@ package com.example.imdbplus;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.example.imdbplus.entity.AccountSetting;
+import com.example.imdbplus.entity.Timeline;
 import com.example.imdbplus.entity.User;
 import com.example.imdbplus.repository.TimelineRepository;
 import com.example.imdbplus.repository.UserRepository;
+import java.util.Objects;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class ImdbPlusApplicationTests {
@@ -37,8 +40,9 @@ class ImdbPlusApplicationTests {
     // Create a test user
     User testUser = new User(testUsername, "testEmail", testAccountSetting);
     userRepository.save(testUser);
-    // Record the userId of the test user
+    // Record the userId and accessToken of the test user
     String testUserId = testUser.getUserId();
+    String testAccessToken = testUser.getAccessToken();
     // Sleep for 1 second to wait for the database to update
     try {
       Thread.sleep(1000);
@@ -48,7 +52,7 @@ class ImdbPlusApplicationTests {
       // Check if the retrieved user is the same as the test user
       assert retrievedUser.equals(testUser);
       // Delete the test user from the database
-      dynamoDBMapper.delete(retrievedUser);
+      userRepository.delete(testUserId, testAccessToken);
     }
   }
 
@@ -65,8 +69,9 @@ class ImdbPlusApplicationTests {
     User testUser = new User(testUsername, "testEmail", testAccountSetting);
     // Save the test user to the database
     userRepository.save(testUser);
-    // Record the userId of the test user
+    // Record the userId and accessToken of the test user
     String testUserId = testUser.getUserId();
+    String testAccessToken = testUser.getAccessToken();
     // Create another test user with the same username
     User testUser2 = new User(testUsername, "testEmail2", testAccountSetting);
 
@@ -84,8 +89,43 @@ class ImdbPlusApplicationTests {
         // Check if the retrieved user is the same as the test user
         assert retrievedUser.equals(testUser);
         // Delete the test user from the database
-        dynamoDBMapper.delete(retrievedUser);
+        userRepository.delete(testUserId, testAccessToken);
       }
+    }
+  }
+
+  /**
+   * Test the timeline save functionality with a single test user and a single test timeline. The
+   * expected behavior is that the test timeline is added to the database.
+   */
+  @Test
+  void testTimelineSave() throws Exception {
+    String testUsername = UUID.randomUUID().toString().replace("-", "") + "-testUsername";
+    AccountSetting testAccountSetting = new AccountSetting(false, true);
+    // Create a test user
+    User testUser = new User(testUsername, "testEmail", testAccountSetting);
+    userRepository.save(testUser);
+    // Record the userId and accessToken of the test user
+    String testUserId = testUser.getUserId();
+    String testAccessToken = testUser.getAccessToken();
+
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException ie) {
+      // Create a test timeline
+      String testMediaId = "tt0000001";
+      String testTimelineId = testUserId + "-" + testMediaId;
+      String testStatus = "DONE";
+      int testRating = 5;
+      String testComment = "This is a test comment";
+      Timeline testTimeline = new Timeline(testTimelineId, testUserId, testMediaId, testStatus,
+          testRating, testComment);
+      ResponseEntity response = timelineRepository.save(testTimeline, testAccessToken);
+      assert response.getStatusCodeValue() == 200;
+      String body = (String) response.getBody();
+      assert Objects.requireNonNull(body).contains("Timeline saved successfully");
+      timelineRepository.delete(testUserId, testMediaId, testAccessToken);
+      userRepository.delete(testUserId, testAccessToken);
     }
   }
 }
