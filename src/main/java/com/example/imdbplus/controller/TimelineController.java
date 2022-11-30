d4df79e46e3eafc78ec62544c38e9b4206ea9e36
Minghan Zhu <mz2716@columbia.edu>
2022-11-30 18:15:06 -0500

    Update TimelineController.java

--- src/main/java/com/example/imdbplus/controller/TimelineController.java
+++ src/main/java/com/example/imdbplus/controller/TimelineController.java
@@ -2,7 +2,9 @@ package com.example.imdbplus.controller;
 
 import com.example.imdbplus.entity.Timeline;
 import com.example.imdbplus.repository.TimelineRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,7 +48,12 @@ public class TimelineController {
   // Get all timelines by userId
   @GetMapping("/timeline/user/{userId}")
   public ResponseEntity getTimeline(@PathVariable("userId") String userId) {
-    return timelineRepository.getTimelineByUserId(userId);
+    List<Timeline> response = timelineRepository.getTimelineByUserId(userId);
+    if (response.size() > 0) {
+      return ResponseEntity.ok(response);
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Timeline not found");
+    }
   }
 
   // Get all timelines by mediaId
