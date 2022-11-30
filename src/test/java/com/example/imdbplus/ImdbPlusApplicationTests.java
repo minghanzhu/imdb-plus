b928483814c79cb6b690d5346c051078ffe92ad2
Minghan Zhu <mz2716@columbia.edu>
2022-11-30 03:17:25 -0500

    Update ImdbPlusApplicationTests.java

--- src/test/java/com/example/imdbplus/ImdbPlusApplicationTests.java
+++ src/test/java/com/example/imdbplus/ImdbPlusApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.imdbplus;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
 import com.example.imdbplus.entity.AccountSetting;
 import com.example.imdbplus.entity.Timeline;
 import com.example.imdbplus.entity.User;
@@ -11,14 +10,14 @@ import com.example.imdbplus.repository.TimelineRepository;
 import com.example.imdbplus.repository.UserRepository;
 import java.util.Objects;
 import java.util.UUID;
-import org.junit.jupiter.api.AfterAll;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
@@ -33,11 +32,11 @@ class ImdbPlusApplicationTests {
   @Autowired
   private TimelineRepository timelineRepository;
 
-  public static User testUser;
-  public static String testUsername;
-  public static AccountSetting testAccountSetting;
-  public static String testUserId;
-  public static String testAccessToken;
+  public static User testUser = new User();
+  public static String testUsername = "testUser";
+  public static AccountSetting testAccountSetting = new AccountSetting();
+  public static String testUserId = "";
+  public static String testAccessToken = "";
 
   @Test
   @Order(1)
@@ -87,16 +86,25 @@ class ImdbPlusApplicationTests {
   @Test
   @Order(4)
   void testGetUser() {
-    User retrievedUser = userRepository.getUser(testUserId);
-    System.out.println();
-    assert retrievedUser.equals(testUser);
+    try {
+      // Sleep for 1 second to wait for the user to be saved to the database
+      Thread.sleep(1000);
+    } catch (Exception e) {
+      User retrievedUser = userRepository.getUser(testUserId);
+      assert retrievedUser.equals(testUser);
+    }
   }
 
   @Test
   @Order(5)
   void testGetUserNotFound() {
-    User retrievedUser = userRepository.getUser("non-existing-user-id");
-    assert retrievedUser == null;
+    try {
+      // Sleep for 1 second to wait for the user to be saved to the database
+      Thread.sleep(1000);
+    } catch (Exception e) {
+      User retrievedUser = userRepository.getUser("testUserId");
+      assert retrievedUser == null;
+    }
   }
 
   /**
@@ -113,18 +121,26 @@ class ImdbPlusApplicationTests {
       String testComment = "This is a test comment";
       Timeline testTimeline = new Timeline(testTimelineId, testUserId, testMediaId, testStatus,
           testRating, testComment);
-      Timeline response = timelineRepository.save(testTimeline, testAccessToken);
-      System.out.println(response);
-      System.out.println(testTimeline);
-      assert response.equals(testTimeline);
-      // Clean up the test timeline
-      timelineRepository.delete(testUserId, testMediaId, testAccessToken);
+      try {
+        // Sleep for 1 second to wait for the timeline to be saved to the database
+        Thread.sleep(1000);
+      } catch (Exception e) {
+        Timeline response = timelineRepository.save(testTimeline, testAccessToken);
+        assert response.equals(testTimeline);
+        // Clean up the test timeline
+        timelineRepository.delete(testUserId, testMediaId, testAccessToken);
+      }
   }
 
   @Test
   @Order(7)
-  void testDeleteUser(){
-    String deleteResult = userRepository.delete(testUserId, testAccessToken);
-    assert deleteResult.equals("User deleted successfully");
+  void testDeleteUser() {
+    try {
+      // Sleep for 1 second to wait for the user to be saved to the database
+      Thread.sleep(1000);
+    } catch (Exception e) {
+      String deleteResult = userRepository.delete(testUserId, testAccessToken);
+      assert deleteResult.equals("User deleted successfully");
+    }
   }
 }
