ee5636d12b098ab393e2c5cd4d257b1eebf74400
Chi Lee <cloud0709@MacBook-Pro-199.local>
2022-12-04 14:44:55 -0800

    Added frontend html and web controller

---
+++ src/main/java/org/opencsd/imdbplus/controller/WebController.java
@@ -0,0 +1,63 @@
+package org.opencsd.imdbplus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping(value = "/home")
+    public String homePage() {
+        return "home";
+    }
+
+    @GetMapping(value = "/user-option")
+    public String userOptionPage() {
+        return "user_option";
+    }
+
+    @GetMapping(value = "/timeline-option")
+    public String timelineOptionPage() {
+        return "timeline_option";
+    }
+
+    @GetMapping(value = "/user-get")
+    public String userGetPage() { return "user_get"; }
+
+    @GetMapping(value = "/user-post")
+    public String userPostPage() {
+        return "user_post";
+    }
+
+    @GetMapping(value = "/user-delete")
+    public String userDeletePage() {
+        return "user_delete";
+    }
+
+    @GetMapping(value = "/timeline-post")
+    public String timelinePostPage() {
+        return "timeline_post";
+    }
+
+    @GetMapping(value = "/timeline-get-by-user-id")
+    public String timelineGETByUserIdPage() {
+        return "timeline_get_user";
+    }
+
+    @RequestMapping(value = "/timeline-get-by-media-id")
+    public String timelineGETByMediaIdPage() {
+        return "timeline_get_media";
+    }
+
+    @RequestMapping(value = "/timeline-get-by-user-media-id")
+    public String timelineGETByUserMediaIdPage() {
+        return "timeline_get_search";
+    }
+
+    @RequestMapping(value = "/timeline-delete")
+    public String timelineDeletePage() {
+        return "timeline_delete";
+    }
+
+}
---
+++ src/test/java/org/opencsd/imdbplus/controller/WebControllerTest.java
@@ -0,0 +1,103 @@
+package org.opencsd.imdbplus.controller;
+
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(WebController.class)
+class WebControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void homePage() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+
+    @Test
+    void userOptionPage() throws Exception {
+        mockMvc.perform(get("/user-option"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user_option"));
+    }
+
+    @Test
+    void timelineOptionPage() throws Exception {
+        mockMvc.perform(get("/timeline-option"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("timeline_option"));
+    }
+
+    @Test
+    void userGetPage() throws Exception {
+        mockMvc.perform(get("/user-get"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user_get"));
+    }
+
+    @Test
+    void userPostPage() throws Exception {
+        mockMvc.perform(get("/user-post"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user_post"));
+    }
+
+
+    @Test
+    void userDeletePage() throws Exception {
+        mockMvc.perform(get("/user-delete"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user_delete"));
+    }
+
+    @Test
+    void timelinePostPage() throws Exception {
+        mockMvc.perform(get("/timeline-post"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("timeline_post"));
+    }
+
+    @Test
+    void timelineGETByUserIdPage() throws Exception {
+        mockMvc.perform(get("/timeline-get-by-user-id"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("timeline_get_user"));
+    }
+
+    @Test
+    void timelineGETByMediaIdPage() throws Exception {
+        mockMvc.perform(get("/timeline-get-by-media-id"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("timeline_get_media"));
+    }
+
+    @Test
+    void timelineGETByUserMediaIdPage() throws Exception {
+        mockMvc.perform(get("/timeline-get-by-user-media-id"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("timeline_get_search"));
+    }
+
+    @Test
+    void timelineDeletePage() throws Exception {
+        mockMvc.perform(get("/timeline-delete"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("timeline_delete"));
+    }
+}
